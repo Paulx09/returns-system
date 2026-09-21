da8c15ab7dbf19fd43972bd8d50be1ce2b6a6237
Roddorr2 <rodricu0703@gmail.com>
2026-09-21 14:11:13 -0500

    fix(auth): add email_verified_at column to users table and update schema

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -14,6 +15,8 @@ class User extends Authenticatable
     use HasFactory, Notifiable, HasUuids, SoftDeletes;
 
     protected $primaryKey = 'user_id';
+    protected $keyType = 'string';
+    public $incrementing = false;
 
     /**
      * The attributes that are mass assignable.
@@ -22,7 +25,9 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'full_name',
+        'name',
         'email',
+        'email_verified_at',
         'password_hash',
         'role',
     ];
@@ -35,6 +40,39 @@ class User extends Authenticatable
     protected $hidden = [
         'password_hash',
     ];
+
+    /**
+     * Interact with the user's id.
+     */
+    protected function id(): Attribute
+    {
+        return Attribute::make(
+            get: fn (): string => (string) $this->getKey(),
+        );
+    }
+
+    /**
+     * Interact with the user's name.
+     */
+    protected function name(): Attribute
+    {
+        return Attribute::make(
+            get: fn (?string $value): string => $this->attributes['full_name'] ?? '',
+            set: fn (string $value): array => ['full_name' => $value],
+        );
+    }
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+        ];
+    }
 
     /**
      * Get the password for the user.
